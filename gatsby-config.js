e24f7b27afc11f18c16d0628ed039a13fe1cd154
ceosss <sswarajsamant@gmail.com>
2020-09-14 21:00:56 +0530

    UPDATED: Strapi Hosted URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,8 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        // apiURL: `http://localhost:1337`,
+        apiURL: `https://ceo-sss-api.herokuapp.com`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`blogs`],
       },
